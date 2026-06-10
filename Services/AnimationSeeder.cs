92df9c4fe7d9f9eed560e2fbf9cbe0d67ddf35fd
Mo7amedghareb <modyghareb2017@gmail.com>
2026-06-11 01:46:41 +0300

    add seeder to add animation files automatically

--- Services/AnimationSeeder.cs
+++ Services/AnimationSeeder.cs
@@ -1,58 +1,8 @@
 ﻿// --------------------- Mg13 ---------------------------
 
-
-//using Talkable.Data.Models;
-//namespace Talkable.Services
-//{
-//    public class AnimationSeeder
-//    {
-//        private readonly MainContext _context;
-//        private readonly IWebHostEnvironment _env;
-
-//        public AnimationSeeder(MainContext context, IWebHostEnvironment env)
-//        {
-//            _context = context;
-//            _env = env;
-//        }
-
-//        public async Task SeedAnimationsAsync()
-//        {
-//            var path = Path.Combine(_env.WebRootPath, "Animations");
-
-//            if (!Directory.Exists(path))
-//                return;
-
-//            var files = Directory.GetFiles(path, "*.glb");
-
-//            var existingSigns = _context.Tb_Signs
-//                .Select(s => s.Name)
-//                .ToHashSet();
-
-//            foreach (var file in files)
-//            {
-//                var fileName = Path.GetFileNameWithoutExtension(file);
-
-//                if (!existingSigns.Contains(fileName))
-//                {
-//                    _context.Tb_Signs.Add(new Signs
-//                    {
-//                        Name = fileName,
-//                        AnimationPath = $"/Animations/{fileName}.glb"
-//                    });
-//                }
-//            }
-
-//            await _context.SaveChangesAsync();
-//        }
-//    }
-//}
-
-
-// --------------------- Mg13 ---------------------------
-
 using Talkable.Data.Models;
 using Microsoft.AspNetCore.Hosting;
-using Talkable.Data.Entities; // للتأكد من قراءة المكتبة بشكل صحيح
+using Talkable.Data.Entities; 
 
 namespace Talkable.Services
 {
@@ -69,32 +19,31 @@ namespace Talkable.Services
 
         public async Task SeedAnimationsAsync()
         {
-            //// ✅ تم التعديل هنا: نستخدم ContentRootPath ونوجهه مباشرة لمجلد wwwroot/Animations
-            //var path = Path.Combine(_env.ContentRootPath, "wwwroot", "Animations");
+            var path = Path.Combine(_env.ContentRootPath, "wwwroot", "Animations");
 
-            //if (!Directory.Exists(path))
-            //    return;
+            if (!Directory.Exists(path))
+                return;
 
-            //var files = Directory.GetFiles(path, "*.glb");
+            var files = Directory.GetFiles(path, "*.glb");
 
-            //var existingSigns = 
-            //    _context.Tb_Signs
-            //    .Select(s => s.Name)
-            //    .ToHashSet();
+            var existingSigns =
+                _context.Tb_Signs
+                .Select(s => s.Name)
+                .ToHashSet();
 
-            //foreach (var file in files)
-            //{
-            //    var fileName = Path.GetFileNameWithoutExtension(file);
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file);
 
-            //    if (!existingSigns.Contains(fileName))
-            //    {
-            //        _context.Tb_Signs.Add(new Signs
-            //        {
-            //            Name = fileName,
-            //            AnimationPath = $"/Animations/{fileName}.glb"
-            //        });
-            //    }
-            //}
+                if (!existingSigns.Contains(fileName))
+                {
+                    _context.Tb_Signs.Add(new Signs
+                    {
+                        Name = fileName,
+                        AnimationPath = $"/Animations/{fileName}.glb"
+                    });
+                }
+            }
 
             await _context.SaveChangesAsync();
         }
