be48ba35df291d222796c294a1c62f719b47e27a
Mo7amedghareb <modyghareb2017@gmail.com>
2026-06-12 21:20:38 +0300

    add isactive column to ghareb database

---
+++ Migrations/20260612173752_Add_column_isactived_to_ghareb_database.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Talkable.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_column_isactived_to_ghareb_database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActived",
+                table: "Tb_Users",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActived",
+                table: "Tb_Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
